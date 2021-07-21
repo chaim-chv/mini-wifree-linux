9aba1d229e03e93360784b432a0890400ce13a28
chv <61952908+chaim-chv@users.noreply.github.com>
2021-07-21 16:36:02 +0300

    add Local DNS server implementation
    it answers (by google DNS server) only to netfree domains ('netfree.link' or any '*.netfree.link')

--- index.js
+++ index.js
@@ -4,14 +4,41 @@ const dns = require('node_dns_changer')
 
 withDNS(axios)
 
-let internetStatus = false
+// local DNS server package
+const dns2 = require('dns2');
+const { Packet } = dns2;
+
+// for netfree adresess we will resolve DNS with google
+const googleDNS = new dns2({ dns: '8.8.8.8', port: 53, recursive: true });
+
+// local DNS server
+const server = dns2.createServer({
+  udp: true,
+  recursive: true,
+  handle: async (request, send, rinfo) => {
+    const response = Packet.createResponseFromRequest(request);
+    const [ question ] = request.questions;
+    const { name } = question;
+    if (name === 'netfree.link' || name.endsWith('.netfree.link')) {
+      const DNSresult = await googleDNS.resolveA(name);
+      response.answers = DNSresult.answers;
+    }
+    else {
+      response.answers = [];
+    }
+    send(response);
+  }
+});
+server.listen({ udp: 53 })
+
+let internetStatus = true;
 
 const disableInternet = async () => {
   await dns.setDNSservers({
-    DNSservers: ['127.0.0.1', '127.0.0.2']
+    DNSservers: ['127.0.0.1']
   })
   internetStatus = false
-  console.log('Internet disabled.')
+  console.log('Internet disabled (except netfree URLs).')
 }
 
 const enableInternet = async () => {
@@ -42,7 +69,10 @@ const detectNetfree = async () => {
     }
   }
 }
-
 detectNetfree()
 
 setInterval(detectNetfree, 1000)
+
+server.on('listening', () => { console.log('local DNS server lauched 🚀') })
+
+server.on('close', () => { console.log('local DNS server DOWN') })
